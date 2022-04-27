5f15eb202e4892f8f91d42a1fbbf3367455860e8
Fernando de Lafuente <fernando.delafuenteq@gmail.com>
2022-04-27 11:29:05 -0500

    Step 4: Beggining the express server

---
+++ app.js
@@ -0,0 +1,9 @@
+// Importando a express
+const express = require("express")
+// App de Express
+const app = express()
+// Indicamos que usaremos JSON
+app.use(express.json())
+//Puerto en que vamos a ver nuestra app: localhost:3000
+const port = 3000
+
